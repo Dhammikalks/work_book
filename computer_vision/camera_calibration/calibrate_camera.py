f6199dac92eeacc37d97a38cb7721750bce2874b
Dhammikalks <dhammika.lks@gmail.com>
2019-07-20 16:22:52 +0530

    adding camera calibration

--- computer_vision/camera_calibration/calibrate_camera.py
+++ computer_vision/camera_calibration/calibrate_camera.py
@@ -5,15 +5,17 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import glob
 
-images = glob.glob('./calibration_images/calibration*.log')
+images = glob.glob('./calibration_images/calibration*.jpg')
 #plt.imshow(img)
 objpoints = []
 imgpoints = []
 
 objp = np.zeros((6*8,3),np.float32)
-objp[:,:,2] = np.mggrid[0:8,0:6].T.reshape(-1,2)
+
+objp[:,:2] = np.mgrid[0:8,0:6].T.reshape(-1,2)
 
 for fname in images:
+    print("got image")
     img = mpimg.imread(fname)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
@@ -24,4 +26,15 @@ for fname in images:
         objpoints.append(objp)
 
         img = cv2.drawChessboardCorners(img,(8,6), corners, ret)
-        plt.imshow(img)
+img = cv2.imread('./calibration_images/test_image.jpg')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+dst = cv2.undistort(img, mtx, dist, None, mtx)
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
+ax1.imshow(img)
+ax1.set_title('Original Image', fontsize=50)
+ax2.imshow(dst)
+ax2.set_title('Undistorted Image', fontsize=50)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+plt.show()
