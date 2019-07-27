2d11bbaaebb2b0d4468c9a50a547a08677bb64ad
Dhammikalks <dhammika.lks@gmail.com>
2019-07-27 16:13:57 +0530

    neural network

--- Neural_Networks/Introduction_to_TensorFlow/Basic/test4.py
+++ Neural_Networks/Introduction_to_TensorFlow/Basic/test4.py
@@ -24,7 +24,7 @@ init = tf.global_variables_initializer()
 
 
 with tf.Session() as sess:
-    file_writer = tf.summary.FileWriter('/home/ros/Desktop/work_book/Neural_Networks/Introduction_to_TensorFlow/Basic/graph',sess.graph)
+    file_writer = tf.summary.FileWriter('/home/dhammika/Desktop/SLDCN/Neural_Networks/Introduction_to_TensorFlow/Basic/graph',sess.graph)
     sess.run(init)
     for i in range(1000):
         sess.run(train,{x: [1,2,3,4], y:[-0,-1,-2,-3]})
--- Neural_Networks/Introduction_to_TensorFlow/Basic/usercase.py
+++ Neural_Networks/Introduction_to_TensorFlow/Basic/usercase.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 
 
 def read_dataset():
-    df = pd.read_csv('/home/ros/Desktop/work_book/Neural_Networks/Introduction_to_TensorFlow/Basic/sonar.csv')
+    df = pd.read_csv('/home/dhammika/Desktop/SLDCN/Neural_Networks/Introduction_to_TensorFlow/Basic/sonar.csv')
     X = df[df.columns[0:60]].values
     y = df[df.columns[60]]
 
@@ -31,51 +31,49 @@ X, Y = read_dataset()
 
 x, Y = shuffle(X,Y,random_state=1)
 
-train_x, train_y, test_x, test_y = train_test_split(X,Y,train_size=0.2,random_state=415)
+test_x,train_x, test_y, train_y = train_test_split(X,Y,train_size=0.2,random_state=415)
 
-print(train_x.shape)
-print(train_y.shape)
-print(test_x.shape)
-print(test_y.shape)
+print("train_x",train_x.shape)
+print("train_y",train_y.shape)
+print("test_x",test_x.shape)
+print("test_y",test_y.shape)
 
 learning_rate = 0.3
 training_epochs = 1000
 cost_history = np.empty(shape=[1],dtype=float)
-n_dim = X.shape
+n_dim = X.shape[1]
 print("n_dim", n_dim)
 n_class = 2
-model_path = "/home/ros/Desktop/work_book/Neural_Networks/Introduction_to_TensorFlow/Basic/model/"
+model_path = "//home/dhammika/Desktop/SLDCN/Neural_Networks/Introduction_to_TensorFlow/Basic/model/"
 
 n_hidden_1 = 60
 n_hidden_2 = 60
 n_hidden_3 = 60
 n_hidden_4 = 60
-n_hidden_5 = 60
 
-w = tf.Variable(tf.zeros[n_dim,n_class])
-b = tf.Variable(tf.zeros[n_class])
+x = tf.placeholder(tf.float32, [None, n_dim])
+w = tf.Variable(tf.zeros([n_dim, n_class]))
+b = tf.Variable(tf.zeros([n_class]))
 
 #Input and outputs
-x = tf.placeholder(tf.float32, [None,n_class])
-
-y = tf.placeholder(tf.float32, [None,n_class])
+y_ = tf.placeholder(tf.float32, [None, n_class])
 
 
 def multilayer_perceptorn(x,weights,biases):
 
-    layer_1 = tf.add(tf.matmul(x,weights['h1'],biases['b1']))
+    layer_1 = tf.add(tf.matmul(x, weights['h1']),biases['b1'])
     layer_1 = tf.nn.sigmoid(layer_1)
 
-    layer_2 = tf.add(tf.matmul(layer_1,weights['h2'],biases['b2']))
+    layer_2 = tf.add(tf.matmul(layer_1,weights['h2']),biases['b2'])
     layer_2 = tf.nn.sigmoid(layer_2)
 
-    layer_3 = tf.add(tf.matmul(layer_2,weights['h3'],biases['b3']))
+    layer_3 = tf.add(tf.matmul(layer_2,weights['h3']),biases['b3'])
     layer_3 = tf.nn.sigmoid(layer_3)
 
-    layer_4 = tf.add(tf.matmul(layer_3,weights['h4'],biases['b4']))
+    layer_4 = tf.add(tf.matmul(layer_3,weights['h4']),biases['b4'])
     layer_4 = tf.nn.relu(layer_4)
 
-    out_layer = tf.add(tf.matmul(x,weights['out'],biases['out']))
+    out_layer = tf.add(tf.matmul(layer_4,weights['out']),biases['out'])
     return out_layer
 
 weights = {
@@ -87,17 +85,55 @@ weights = {
 }
 
 biases = {
-     'h1': tf.Variable(tf.truncated_normal([n_hidden_1])),
-     'h2': tf.Variable(tf.truncated_normal([n_hidden_2])),
-     'h3': tf.Variable(tf.truncated_normal([n_hidden_3])),
-     'h4': tf.Variable(tf.truncated_normal([n_hidden_4])),
+     'b1': tf.Variable(tf.truncated_normal([n_hidden_1])),
+     'b2': tf.Variable(tf.truncated_normal([n_hidden_2])),
+     'b3': tf.Variable(tf.truncated_normal([n_hidden_3])),
+     'b4': tf.Variable(tf.truncated_normal([n_hidden_4])),
      'out': tf.Variable(tf.truncated_normal([n_class]))
  }
 init = tf.global_variables_initializer()
 
 saver = tf.train.Saver()
 
-y = multilayer_perceptorn(x,weights,biases)
+y = multilayer_perceptorn(x, weights, biases)
 
-cost_function = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y,logits=y_))
+cost_function = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_ ))
 train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost_function)
+
+
+
+with tf.Session() as sess:
+    file_writer = tf.summary.FileWriter('/home/dhammika/Desktop/SLDCN/Neural_Networks/Introduction_to_TensorFlow/Basic/graph',sess.graph)
+    sess.run(init)
+    mse_history = []
+    accuracy_history = []
+    for epoch in range(training_epochs):
+        sess.run(train_step,feed_dict={x: train_x, y_:train_y})
+        cost = sess.run(cost_function,feed_dict={x:train_x,y_:train_y})
+        cost_history = np.append(cost_history,cost)
+        correct_prediction = tf.equal(tf.argmax(y,1),tf.argmax(y_,1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+        pred_y = sess.run(y,feed_dict={x:test_x})
+        mse = tf.reduce_mean(tf.square(pred_y-test_y))
+        mse_ = sess.run(mse)
+        mse_history.append(mse_)
+        accuracy = sess.run(accuracy,feed_dict={x:test_x,y_:test_y})
+        accuracy_history.append(accuracy)
+
+        print('epoch : ',epoch, '-', 'cost: ',cost, "- MSE ",mse, " - Train Accuracy: ", accuracy)
+
+    save_path = saver.save(sess,model_path)
+    print("Model saved in file: %s" % save_path)
+
+    plt.plot(mse_history, 'r')
+    plt.show()
+    plt.plot(accuracy_history)
+    plt.show()
+
+    correct_prediction = tf.equal(tf.argmax(y, 1),tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+    print("Test Accuracy : " (sess.run(accuracy,feed_dict={x:test_x,y_:test_y})))
+
+    pred_y = sess.run(y,feed_dict={x:test_x})
+    mse = tf.reduce_mean(tf.square(pred_y-test_y))
+    print("MSE : %.4f",sess.run(mse))
