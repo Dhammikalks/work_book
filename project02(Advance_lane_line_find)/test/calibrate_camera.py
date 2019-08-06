4bddcf274b0ba887dd849c121599d1e45b47bfa6
Dhammikalks <dhammika.lks@gmail.com>
2019-08-06 09:53:13 -0700

    converlutional neural network

---
+++ project02(Advance_lane_line_find)/test/calibrate_camera.py
@@ -0,0 +1,87 @@
+#/bin/python -f
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import glob
+
+
+def corners_unwarp(img, nx, ny, mtx, dist):
+    # Use the OpenCV undistort() function to remove distortion
+    undist = cv2.undistort(img, mtx, dist, None, mtx)
+    # Convert undistorted image to grayscale
+    gray = cv2.cvtColor(undist, cv2.COLOR_BGR2GRAY)
+    # Search for corners in the grayscaled image
+    ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
+
+    if ret == True:
+        # If we found corners, draw them! (just for fun)
+        cv2.drawChessboardCorners(undist, (nx, ny), corners, ret)
+        # Choose offset from image corners to plot detected corners
+        # This should be chosen to present the result at the proper aspect ratio
+        # My choice of 100 pixels is not exact, but close enough for our purpose here
+        offset = 100 # offset for dst points
+        # Grab the image shape
+        img_size = (gray.shape[1], gray.shape[0])
+
+        # For source points I'm grabbing the outer four detected corners
+        src = np.float32([corners[0], corners[nx-1], corners[-1], corners[-nx]])
+        # For destination points, I'm arbitrarily choosing some points to be
+        # a nice fit for displaying our warped result
+        # again, not exact, but close enough for our purposes
+        dst = np.float32([[offset, offset], [img_size[0]-offset, offset],
+                                     [img_size[0]-offset, img_size[1]-offset],
+                                     [offset, img_size[1]-offset]])
+        # Given src and dst points, calculate the perspective transform matrix
+        M = cv2.getPerspectiveTransform(src, dst)
+        # Warp the image using OpenCV warpPerspective()
+        warped = cv2.warpPerspective(undist, M, img_size)
+
+    # Return the resulting image and matrix
+    return warped, M
+
+def calibrateCamera(images,nx,ny):
+    objpoints = []
+    imgpoints = []
+    objp = np.zeros((ny*nx,3),np.float32)
+
+    objp[:,:2] = np.mgrid[0:nx,0:ny].T.reshape(-1,2)
+
+    for fname in images:
+        img = mpimg.imread(fname)
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        ret, corners = cv2.findChessboardCorners(gray, (nx,ny),None)
+        if ret == True:
+            imgpoints.append(corners)
+            objpoints.append(objp)
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+    return ret, mtx, dist, rvecs, tvecs
+
+def undistorted_test(img,mtx, dist):
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    dst = cv2.undistort(img, mtx, dist, None, mtx)
+    return dst
+
+def compare_image(img1,img2):
+    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+    f.tight_layout()
+    ax1.imshow(img1)
+    ax1.set_title('Original Image', fontsize=50)
+    ax2.imshow(img2)
+    ax2.set_title('Undistorted Image', fontsize=50)
+    plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+    plt.show()
+
+
+images = glob.glob('./../camera_cal/calibration*.jpg')
+ret, mtx, dist, rvecs, tvecs = calibrateCamera(images,9,6)
+
+#undistort images
+img = cv2.imread('./../camera_cal/test_image.jpg')
+undistorted_image = undistorted_test(img,mtx, dist)
+compare_image(img,undistorted_image)
+
+#change persepective
+#img = cv2.imread('./calibration_images/test_image2.png')
+#warped,M = corners_unwarp(img, 8, 6 , mtx, dist)
+#compare_image(img,warped)
